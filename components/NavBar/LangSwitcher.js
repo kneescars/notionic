95f3ba7301757d40aa06228e4aab34a5ce6f4f5b
Cassiano G. <124882816+kneescars@users.noreply.github.com>
2023-12-02 22:11:12 +0100

    Changing Chinese for French on line 10

--- components/NavBar/LangSwitcher.js
+++ components/NavBar/LangSwitcher.js
@@ -1,5 +1,5 @@
-import { TranslateIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
+import { TranslateIcon } from '@heroicons/react/outline'
 import { useRouter } from 'next/router'
 
 const LangSwitcher = () => {
@@ -7,7 +7,7 @@ const LangSwitcher = () => {
 
   return (
     <>
-      <Link passHref href={asPath} locale={locale === 'en' ? 'zh' : 'en'} scroll={false}>
+      <Link passHref href={asPath} locale={locale === 'en' ? 'fr' : 'en'} scroll={false}>
         <button
           aria-label='LangSwitcher'
           className='p-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100'
