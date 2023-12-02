ebf74f88ff0bfea5e4baffb951fb0a84b973c95b
Cassiano G. <124882816+kneescars@users.noreply.github.com>
2023-12-02 22:05:41 +0100

    Changes to i18n:, changing chinese for French

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   link: 'https://zeitgeist.digital',
   newsletter: 'Digital Zeitgeist',
   description: 'Digital | Media | Tech',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'fr-FR', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Europe/Paris', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
